edf0e3dd36af17cc48f0d896f77757a17ec3f218
testgithub-trial <88606059+testgithub-trial@users.noreply.github.com>
2022-04-13 05:32:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print ('Hii Sudharr')
+
+from datetime import date,datetime
+now = datetime.now()
+today = date.today()
+d2 = today.strftime("%B %d, %Y")
+print("d2 =", d2)
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
